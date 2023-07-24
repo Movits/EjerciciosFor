550d7829c4a8a9be50aa0c93163ec9230ab3f905
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-24 02:31:32 -0300

    ej2v2 finalizado

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,8 +1,8 @@
 const contenedorDiv = document.querySelector("div");
-const VALOR_MAX = 5
+const VALOR_MAX = 30
 
 for (let i = 0; i < VALOR_MAX; i++) {
     contenedorDiv.innerHTML += `
-        <p>hola</p>
+        <p>hola ${i+1}</p>
     `;
 }
